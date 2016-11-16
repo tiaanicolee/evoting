a72ababfcf16d7588bd9399d8ed3ced1561e8362
JoshuaClark311 <Clarkjd@g.cofc.edu>
2016-11-16 18:18:23 -0500

    I/O Handler Update
    Filled in the printing methods, utilizing a scanner. Prints from a flat file that will be created from another class

--- src/IOHandler.java
+++ src/IOHandler.java
@@ -1,20 +1,51 @@
+import java.io.*;
+import java.util.Scanner;
 /**
  * A handler for the IO operations concerning the printer.
  */
-public class IOHandler {
+public class IOHandler {	
 	/**
-	 * handles printing the results
+	 * handles printing the results from a flat file
 	 */
 	public void printResults()
 	{
-		
+		File results = new File("results.txt");
+		try 
+		{
+			Scanner scan = new Scanner(results);
+			
+			while (scan.hasNextLine()) {
+				String line = scan.nextLine();
+				System.out.println(line);
+			}
+			scan.close();			
+		}
+			
+		catch(Exception e){
+			
+		}
 	}
 	
 	/**
-	 * handles printing the recount
+	 * handles printing the recount from a flat file
 	 */
 	public void printRecount()
 	{
-		
+		File recount = new File("recount.txt");
+		try 
+		{
+			Scanner scan = new Scanner(recount);
+			
+			while (scan.hasNextLine()) {
+				String line = scan.nextLine();
+				System.out.println(line);
+			}
+			scan.close();			
+		}
+			
+		catch(Exception e){
+			
+		}
 	}
 }
+
